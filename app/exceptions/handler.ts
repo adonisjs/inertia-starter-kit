5c9b5e82966f94a968c1c914e54396715044f849
Julien Ripouteau <julien@ripouteau.com>
2024-03-08 00:38:02 +0100

    feat: remove edge error page with inertia

--- app/exceptions/handler.ts
+++ app/exceptions/handler.ts
@@ -21,12 +21,8 @@ export default class HttpExceptionHandler extends ExceptionHandler {
    * to return the HTML contents to send as a response.
    */
   protected statusPages: Record<StatusPageRange, StatusPageRenderer> = {
-    '404': (error, { view }) => {
-      return view.render('pages/errors/not_found', { error })
-    },
-    '500..599': (error, { view }) => {
-      return view.render('pages/errors/server_error', { error })
-    },
+    '404': (error, { inertia }) => inertia.render('errors/not_found', { error }),
+    '500..599': (error, { inertia }) => inertia.render('errors/server_error', { error }),
   }
 
   /**
