527931c910f731c5d8bbd915a8d9b8a5374dbdae
Julien Ripouteau <julien@ripouteau.com>
2024-03-08 00:39:48 +0100

    feat: add default alias in vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,16 @@
 import { defineConfig } from 'vite'
+import { getDirname } from '@adonisjs/core/helpers'
 
 export default defineConfig({
   plugins: [],
+
+  /**
+   * Define aliases for importing modules from
+   * your frontend code
+   */
+  resolve: {
+    alias: {
+      '~/': `${getDirname(import.meta.url)}/inertia`,
+    },
+  },
 })
