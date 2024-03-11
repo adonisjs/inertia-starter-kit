e9b7a189ff82885ce0896252a77ad420af4c542a
Julien Ripouteau <julien@ripouteau.com>
2024-03-11 16:03:22 +0100

    fix: `~/` alias not working

--- vite.config.ts
+++ vite.config.ts
@@ -10,7 +10,7 @@ export default defineConfig({
    */
   resolve: {
     alias: {
-      '~/': `${getDirname(import.meta.url)}/inertia`,
+      '~/': `${getDirname(import.meta.url)}/inertia/`,
     },
   },
 })
