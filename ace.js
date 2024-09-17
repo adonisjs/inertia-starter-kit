c04cd9f11c3b55e5a73d52569fca02e0b6015edb
Romain Lanz <romain.lanz@pm.me>
2024-09-17 20:26:34 +0200

    chore: migrate to ts-node-maintained

--- ace.js
+++ ace.js
@@ -17,10 +17,9 @@
 */
 
 /**
- * Register hook to process TypeScript files using ts-node
+ * Register hook to process TypeScript files using ts-node-maintained
  */
-import { register } from 'node:module'
-register('ts-node/esm', import.meta.url)
+import 'ts-node-maintained/register/esm'
 
 /**
  * Import ace console entrypoint
